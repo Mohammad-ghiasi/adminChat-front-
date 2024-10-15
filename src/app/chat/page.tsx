fcbdaba7c4da1530f63b4dd54616eb27a3f4fdd1
mohammad <mohammadghiasi005@gmail.com>
2024-10-15 21:46:51 +0330

    adminChatFront_v-1-0-4

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,33 +1,47 @@
+"use client"
 import ChatBox from '@/components/Chat';
 import { Text } from '@chakra-ui/react';
-import { cookies } from 'next/headers';
 import axios from 'axios';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Cookies from 'js-cookie';
 
-export default async function ChatRoute() {
-  // Fetch the cookies
-  const cookiesStore = cookies();
-  const jsonUserData: string | undefined = await cookiesStore.get('userData')?.value;
-  const userData = jsonUserData ? JSON.parse(jsonUserData) : null;
+export default function ChatRoute() {
+  const [userData, setUserData] = useState<any>(null);
+  const [roomData, setRoomData] = useState<any>(null);
 
-  // Fetch the room data if userData and userId are available
-  let roomData = null;
-  if (userData && userData.userId) {
-    try {
-      await axios.post('https://adminchat-back-3ohq.vercel.app/chat/add-chatRoom', {data: userData.userId})
-      const response = await axios.get(`https://adminchat-back-3ohq.vercel.app/chat/find-chatRoom/${userData.userId}`)
+  useEffect(() => {
+    // Fetch the userData cookie
+    const jsonUserData = Cookies.get('userData');
+    const parsedUserData = jsonUserData ? JSON.parse(jsonUserData) : null;
+    setUserData(parsedUserData);
 
-      // Assuming your endpoint returns room data under 'data'
-      roomData = response?.data.data;
-    } catch (error) {
-      console.error('Error fetching room data:', error);
+    // Fetch the room data if userData and userId are available
+    if (parsedUserData && parsedUserData.userId) {
+      const fetchRoomData = async () => {
+        try {
+          await axios.post('https://adminchat-back-3ohq.vercel.app/chat/add-chatRoom', {
+            data: parsedUserData.userId,
+          });
+
+          const response = await axios.get(
+            `https://adminchat-back-3ohq.vercel.app/chat/find-chatRoom/${parsedUserData.userId}`
+          );
+
+          // Assuming your endpoint returns room data under 'data'
+          setRoomData(response?.data?.data);
+        } catch (error) {
+          console.error('Error fetching room data:', error);
+        }
+      };
+
+      fetchRoomData();
     }
-  }
+  }, []);
 
   return (
     <>
-      <ChatBox room={roomData.room} user={roomData.user}/>
-      {userData && (<Text>I have userData cookie!</Text>)}
+      <ChatBox room={roomData?.room} user={roomData?.user} />
+      {userData && <Text>I have userData cookie!</Text>}
       {/* chatbutton */}
     </>
   );
